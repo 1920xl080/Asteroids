dc44a87c42f6965db480cfb691c63d10701965d0
1920xl080 <arrigo1888@gmail.com>
2025-07-20 16:58:11 +0700

    Final Version of Asteroids Game

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 import pygame
 from constants import *
+from player import *
+from asteroidfield import *
+from circleshape import *
+from asteroid import *
+from shot import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable , drawable)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        for sprite in updatable:
+            sprite.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                pygame.quit()
+                return
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.split()
         screen.fill('black')
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
